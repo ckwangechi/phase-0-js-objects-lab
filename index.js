fa85b45fcf1112ea3bd491672d62feb0f400129d
Christine Karuga <christinewangechik@gmail.com>
2026-03-24 08:05:08 +0300

    add JS code

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 //Write your code here
 
+// Declare and initiliaze attendee object
+const attendee = {
+  id: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Confrence",
+  ticketType: "VIP",
+  ticketPrice: 150.00,
+};
+
+// Log the attendee's name
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+// Log the ticket price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+// Update the ticket type
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+// Update the ticket price
+function updateTicketPrice(attendee, newTicketPrice) {
+  attendee.ticketPrice = newTicketPrice;
+}
+
+// Remove the event property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+// Add a checkedIn property
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 
 //Needed for the tests to work. Don't modify
